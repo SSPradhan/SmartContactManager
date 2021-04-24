e7dbc9f72bfab8dee5c012a9508b3420458036a2
SSPradhan <shubhransushekhar@yahoo.com>
2021-04-24 17:30:00 +0530

    Welcome File done

--- src/main/java/com/bng/repo/UserRepository.java
+++ src/main/java/com/bng/repo/UserRepository.java
@@ -1,5 +1,10 @@
 package com.bng.repo;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bng.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>
+{
 
 }
